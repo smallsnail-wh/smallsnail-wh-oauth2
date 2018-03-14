dd786888ab8c14e40ca108de67b0b7d54f2f7cf3
wangh <wangh@shanshuiyun.cn>
2018-03-14 16:51:44 +0800

    创建项目

---
+++ src/main/java/com/smallsnail/oauth2/SmallsnailWhOauth2Application.java
@@ -0,0 +1,12 @@
+package com.smallsnail.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallsnailWhOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallsnailWhOauth2Application.class, args);
+	}
+}
